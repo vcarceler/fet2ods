15134cea1dd0910f6fa695bc66ff51949345ff61
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2019-07-26 12:48:06 +0200

    Lectura teachers.xml

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,40 @@
 //
 // Es una herramienta sencilla para iniciarse en la programación con Rust.
 
+extern crate xml;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size).map(|_| INDENT)
+        .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("/home/vcarceler/desarrollo/fet2ods/data/teachers.xml").unwrap();
+    let file = BufReader::new(file);
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{}+{}", indent(depth), name);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{}-{}", indent(depth), name);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
 }
