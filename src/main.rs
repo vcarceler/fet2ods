f636651eac5031a9aa432e70ae67eb67cb314e39
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2019-07-26 12:20:00 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+// This file is part of fet2ods.
+//
+// fet2ods is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// fet2ods is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with fet2ods.  If not, see <https://www.gnu.org/licenses/>.
+
+// Copyright 2019 Victor Carceler
+
+
+// Este programa utiliza el fichero data/teachers.xml generado por FET
+// (https://lalescu.ro/liviu/fet/features.html) para generar hojas
+// de cálculo .ods con los horarios de los profesores.
+//
+// Es una herramienta sencilla para iniciarse en la programación con Rust.
+
+fn main() {
+    println!("Hello, world!");
+}
